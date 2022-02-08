9a8bd33057c58d81e93c2570f3e4e2263b989d56
Luke Hall <luhall@microsoft.com>
2022-02-07 16:13:59 -0800

    Pushing all changes

--- WalkerEngine/src/Renderer/Shader.cpp
+++ WalkerEngine/src/Renderer/Shader.cpp
@@ -16,4 +16,34 @@ namespace Walker {
 		return nullptr;
     }
 
+	void ShaderLibrary::Add(const std::string& name, const std::shared_ptr<Shader>& shader)
+	{
+		//W_CORE_ASSERT(!Exists(name), "Shader already exists!");
+		m_Shaders[name] = shader;
+	}
+
+	void ShaderLibrary::Add(const std::shared_ptr<Shader>& shader)
+	{
+		auto& name = shader->GetName();
+		Add(name, shader);
+	}
+
+	std::shared_ptr<Shader> ShaderLibrary::Load(const std::string& name, const std::string& vertexPath, const std::string& fragmentPath, const std::string& geometryPath)
+	{
+		auto shader = Shader::Create(name, vertexPath, fragmentPath, geometryPath);
+		Add(shader);
+		return shader;
+	}
+
+	std::shared_ptr<Shader> ShaderLibrary::Get(const std::string& name)
+	{
+		//W_CORE_ASSERT(Exists(name), "Shader not found!");
+		return m_Shaders[name];
+	}
+
+	bool ShaderLibrary::Exists(const std::string& name) const
+	{
+		return m_Shaders.find(name) != m_Shaders.end();
+	}
+
 }
--- WalkerEngine/src/Renderer/Shader.h
+++ WalkerEngine/src/Renderer/Shader.h
@@ -40,8 +40,7 @@ namespace Walker {
 	public:
 		void Add(const std::string& name, const std::shared_ptr<Shader>& shader);
 		void Add(const std::shared_ptr<Shader>& shader);
-		std::shared_ptr<Shader> Load(const std::string& filepath);
-		std::shared_ptr<Shader> Load(const std::string& name, const std::string& filepath);
+		std::shared_ptr<Shader> Load(const std::string& name, const std::string& vertexPath, const std::string& fragmentPath, const std::string& geometryPath = "");
 
 		std::shared_ptr<Shader> Get(const std::string& name);
 
